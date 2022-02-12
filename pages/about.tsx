effc349226e7e6e5429ce0dbdb5f31cd80536545
Omer Can Yigit <omercan.yigit@dinodream.com>
2022-02-12 16:48:06 +0300

    git init

---
+++ pages/about.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const About: NextPage = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+About.defaultProps = {
+  data: []
+}
+
+export default About
---
+++ pages/product/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Product: NextPage = () => {
+  return (
+    <div>Product</div>
+  )
+}
+
+Product.defaultProps = {
+  data: []
+}
+
+export default Product
