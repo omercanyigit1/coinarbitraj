d0451120867733320be92976683bf7e262dcff7e
Omer Can Yigit <omercan.yigit@dinodream.com>
2022-02-12 17:44:51 +0300

    m

---
+++ src/pages/about.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import {
+  useAppDispatch,
+  useAppSelector,
+} from '../app/hooks';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  selectCount,
+} from '../features/counter/counterSlice';
+
+const IndexPage:React.FC = () => {
+  const dispatch = useAppDispatch();
+  const count = useAppSelector(selectCount);
+  const [incrementAmount, setIncrementAmount] = useState<number>(0);
+
+  return (
+    <>
+      <h1>Welcome to the greatest app in the world!</h1>
+      <h2>
+        The current number is
+        {count}
+      </h2>
+      <div>
+        <input
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(Number(e.target.value))}
+          type="number"
+        />
+        <button
+          onClick={() =>        dispatch(incrementByAmount(Number(incrementAmount)))}
+        >
+          Increment by amount
+        </button>
+      </div>
+      <div>
+        <button onClick={() => dispatch(decrement())}>Decrement by 1</button>
+        <button onClick={() => dispatch(increment())}>Increment by 1</button>
+      </div>
+    </>
+  );
+};
+
+export default IndexPage;
